51615cf318fe6a6c3aba0692864e2cd453d6faba
kaanuznr <kaanuznr@gmail.com>
2024-05-16 16:02:21 +0300

    file_path variable made public

--- tests/uniq_tests.rs
+++ tests/uniq_tests.rs
@@ -4,38 +4,34 @@ mod tests {
 
     use uniq::UNIQ;
 
+    pub const FILE_PATH: PathBuf = PathBuf::from("test_files/test.txt");
     #[test]
     fn test_print_lines() {
-        let file_path = PathBuf::from("test_files/test.txt");
-        let uniq = UNIQ::new(file_path);
+        let uniq = UNIQ::new(FILE_PATH);
         uniq.print_lines();
     }
 
     #[test]
     fn test_print_uniq() {
-        let file_path = PathBuf::from("test_files/test.txt");
-        let uniq = UNIQ::new(file_path);
+        let uniq = UNIQ::new(FILE_PATH);
         uniq.print_uniq();
     }
 
     #[test]
     fn test_print_duplicates() {
-        let file_path = PathBuf::from("test_files/test.txt");
-        let uniq = UNIQ::new(file_path);
+        let uniq = UNIQ::new(FILE_PATH);
         uniq.print_duplicates();
     }
 
     #[test]
     fn test_count_unique_lines() {
-        let file_path = PathBuf::from("test_files/test.txt");
-        let uniq = UNIQ::new(file_path);
+        let uniq = UNIQ::new(FILE_PATH);
         uniq.count_unique_lines();
     }
 
     #[test]
     fn test_print_counters() {
-        let file_path = PathBuf::from("test_files/test.txt");
-        let uniq = UNIQ::new(file_path);
+        let uniq = UNIQ::new(FILE_PATH);
         uniq.print_counters(); 
     }
 }
